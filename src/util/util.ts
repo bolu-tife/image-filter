52346fa55457bf6930bca86c21f6f60b1962ded2
Boluwatife Omosowon <boluomosowon@gmail.com>
2022-09-22 14:31:11 +0100

    added filteredimage endpoint

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,17 +1,22 @@
+import axios from "axios";
 import fs from "fs";
-import Jimp = require("jimp");
+import Jimp from "jimp";
 
-// filterImageFromURL
-// helper function to download, filter, and save the filtered image locally
-// returns the absolute path to the local image
-// INPUTS
-//    inputURL: string - a publicly accessible url to an image file
-// RETURNS
-//    an absolute path to a filtered image locally saved file
+/**
+ * filterImageFromURL -
+ * helper function to download, filter, and save the filtered image locally
+ * returns the absolute path to the local image
+ * Returns an absolute path to a filtered image locally saved file
+ *
+ *
+ * @param inputURL - a publicly accessible url to an image file
+ * @returns an absolute path to a filtered image locally saved file
+ */
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const imageBuffer = await getImageBuffer(inputURL);
+      const photo = await Jimp.read(imageBuffer);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
@@ -27,11 +32,33 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
   });
 }
 
-// deleteLocalFiles
-// helper function to delete files on the local disk
-// useful to cleanup after tasks
-// INPUTS
-//    files: Array<string> an array of absolute paths to files
+/**
+ * getImageBuffer -
+ * helper function to get image buffer
+ *
+ * returns an imagebuffer
+ *
+ * @param inputURL - a publicly accessible url to an image file
+ * @returns an imagebuffer
+ */
+export async function getImageBuffer(inputURL: string) {
+  const image = await axios({
+    method: "get",
+    url: inputURL,
+    responseType: "arraybuffer",
+  });
+  return image.data;
+}
+
+/**
+ * deleteLocalFiles
+ * helper function to delete files on the local disk
+ * useful to cleanup after tasks
+ *
+ * returns an imagebuffer
+ *
+ * @param files - Array<string> an array of absolute paths to files
+ */
 export async function deleteLocalFiles(files: Array<string>) {
   for (let file of files) {
     fs.unlinkSync(file);
